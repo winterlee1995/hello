088a3f32264714917b0ecb661d66855e6dccb632
winter-Lee <winterlee1995@163.com>
2020-08-12 13:05:28 +0800

    第一个hello test

--- src/Student.java
+++ src/Student.java
@@ -12,6 +12,6 @@ public class Student {
 
     public static void main(String[] args) {
         System.out.println("I am a student ,my name is winter-lee");
-        //System.out.println("second");
+        System.out.println("second");
     }
 }
