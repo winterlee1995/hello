a283c393add07154efbba6c7758d8f191049f3bc
winter-Lee <winterlee1995@163.com>
2020-08-12 13:00:51 +0800

    第一个hello test

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I am a student ,my name is winter-lee");
+        //System.out.println("second");
+    }
+}
